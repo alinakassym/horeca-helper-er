18ab0ee124b3c6be018372b38a0a07509ca8699f
Alina <alinakassym@gmail.com>
2021-10-19 13:59:36 +0600

    JobService

---
+++ src/services/JobsService.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+// emulator
+const baseUrl = Platform.OS === 'android' ? 'http://10.0.2.2' : 'http://localhost';
+
+// android device
+// const baseUrl = 'http://localhost';
+
+const port = '3000';
+
+export const getJobs = async (hhToken) => {
+  const r = await axios.get('' +
+    `${baseUrl}:${port}/er/jobs`, {
+    headers: {'Authorization': `Bearer ${hhToken || ''}`}
+  });
+  console.log('getJobs result: ', r.data)
+  return r;
+};
+
+export const getJobById = async (id, hhToken) => {
+  const r = await axios.get('' +
+    `${baseUrl}:${port}/er/jobs/${id}`, {
+    headers: {'Authorization': `Bearer ${hhToken || ''}`}
+  });
+  console.log('getJobById result: ', r.data)
+  return r;
+};
+
+export const updateJobById = async (id, data, hhToken) => {
+  const r = await axios.patch('' +
+    `${baseUrl}:${port}/er/jobs/${id}`, data, {
+    headers: {'Authorization': `Bearer ${hhToken || ''}`}
+  });
+  console.log('updateJobById result: ', r.data)
+  return r;
+};
+
+export const postJob = async (data, hhToken) => {
+  console.log('job item : ', data);
+  const r = await axios.post('' +
+    `${baseUrl}:${port}/er/jobs`, data, {
+    headers: {'Authorization': `Bearer ${hhToken || ''}`}
+  });
+  console.log('postJob result: ', r)
+  return r;
+};
