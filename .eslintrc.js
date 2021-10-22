5cd4fa46dd9bbcd2a0590b284c467ef9d58763d3
Alina <alinakassym@gmail.com>
2021-10-22 15:19:29 +0600

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['plugin:react-hooks/recommended', '@react-native-community'],
+  env: {
+    'react-native/react-native': true,
+  },
+  plugins: ['react', 'react-native'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
 };
