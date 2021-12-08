494bd9c30d7f6b950760d729171e13488838486b
Alina <alinakassym@gmail.com>
2021-12-08 22:29:34 +0600

    Chat: search message

--- src/screens/messages/MessagesScreen.js
+++ src/screens/messages/MessagesScreen.js
@@ -2,17 +2,33 @@ import React, {useEffect, useState} from 'react';
 import {ScrollView, View, SafeAreaView, StyleSheet} from 'react-native';
 import {globalStyles} from '../../styles/globalStyles';
 import {MessagePreview} from './components/MessagePreview';
-import {getChats} from '../../services/ChatService';
+import {getChats, getChatsSearch} from '../../services/ChatService';
 import {MessageSearch} from './components/MessageSearch';
 
 export const MessagesScreen = ({navigation}) => {
   const [chats, setChats] = useState([]);
+  const [searchText, setSearchText] = useState('');
+
+  const searchMessage = async () => {
+    try {
+      if (searchText.length > 0) {
+        const res = await getChatsSearch(searchText);
+        setChats(res);
+      } else {
+        const res = await getChats();
+        setChats(res);
+      }
+    } catch (e) {
+      console.log('searchMessage err: ', e);
+    }
+  };
 
   useEffect(() => {
     return navigation.addListener('focus', async () => {
       try {
         const res = await getChats();
         setChats(res);
+        setSearchText('');
       } catch (e) {
         console.error('MessagesScreen err: ', e);
       }
@@ -20,7 +36,11 @@ export const MessagesScreen = ({navigation}) => {
   }, [navigation]);
   return (
     <SafeAreaView style={globalStyles.container}>
-      <MessageSearch />
+      <MessageSearch
+        text={searchText}
+        onChangeText={val => setSearchText(val)}
+        onEndEditing={() => searchMessage()}
+      />
       <ScrollView style={styles.scrollView}>
         <View style={styles.chats}>
           {chats.map((item, index) => (
--- src/screens/messages/components/MessageSearch.js
+++ src/screens/messages/components/MessageSearch.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {View, StyleSheet, Text, TextInput} from 'react-native';
 import {IconSearch} from '../../../assets/icons/tabs/IconSearch';
+import {IconClose} from '../../../assets/icons/main/IconClose';
 
-export const MessageSearch = () => {
+export const MessageSearch = ({text, onChangeText, onEndEditing}) => {
   return (
     <View style={styles.search}>
       <Text style={styles.header}>
@@ -11,13 +12,22 @@ export const MessageSearch = () => {
       </Text>
       <View style={styles.inputSection}>
         <TextInput
+          value={text}
           style={styles.input}
           placeholder={'Search'}
           placeholderTextColor={'#B9C1CA'}
+          onChangeText={onChangeText}
+          onEndEditing={onEndEditing}
         />
         <View style={styles.iconSearch}>
           <IconSearch size={24} color={'#B9C1CA'} />
         </View>
+
+        {text.length > 0 && (
+          <View style={styles.iconClear}>
+            <IconClose size={12} color={'#8391A1'} width={2} />
+          </View>
+        )}
       </View>
     </View>
   );
@@ -49,7 +59,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     fontFamily: 'Inter-Regular',
     fontSize: 16,
-    lineHeight: 14,
+    lineHeight: 24,
     backgroundColor: '#F0F2F4',
     color: '#151F47',
   },
@@ -57,5 +67,14 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 12,
     left: 12,
+  },
+  iconClear: {
+    position: 'absolute',
+    top: 16,
+    right: 12,
+    padding: 2,
+    borderRadius: 24,
+    borderWidth: 1,
+    borderColor: '#8391A1',
   },
 });
