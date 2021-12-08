baf2f0b494e53959c1f66de0113ede73d052c988
Alina <alinakassym@gmail.com>
2021-12-08 22:28:37 +0600

    Services: get chats search

--- src/services/ChatService.js
+++ src/services/ChatService.js
@@ -2,7 +2,7 @@ import http from '../http-common';
 
 export const getChats = async () => {
   const r = await http.get('/er/chats');
-  console.log('getChats: ', r.data);
+  // console.log('getChats: ', r.data);
   return r.data;
 };
 
@@ -20,5 +20,12 @@ export const getChatById = async id => {
 
 export const postMessage = async (id, data) => {
   const r = await http.post(`/er/chats/${id}/messages`, data);
+  return r.data;
+};
+
+export const getChatsSearch = async data => {
+  const r = await http.get(`/er/chats/search?term=${data}`);
+  console.log('data: ', data);
+  console.log('getChatsSearch: ', r.data);
   return r.data;
 };
