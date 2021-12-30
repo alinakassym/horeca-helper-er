25c08ebd956480582e4e482a81d24a15ab6e78ba
Alina <alinakassym@gmail.com>
2021-12-30 16:34:07 +0600

    Icons: add style prop

--- src/assets/icons/main/IconAdd.js
+++ src/assets/icons/main/IconAdd.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import Svg, {Path} from 'react-native-svg';
 
-export const IconAdd = ({color, size, width}) => {
+export const IconAdd = ({color, size, width, style}) => {
   return (
-    <Svg width={size || 16} height={size || 16} fill="none">
+    <Svg style={style} width={size || 16} height={size || 16} fill="none">
       <Path
         d="M8 2v6m0 0h6M8 8H2m6 0v6"
         stroke={color || '#000000'}
--- src/assets/icons/main/IconDot.js
+++ src/assets/icons/main/IconDot.js
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import Svg, {Circle} from 'react-native-svg';
 
-export const IconDot = ({color, size}) => {
+export const IconDot = ({color, size, style}) => {
   return (
-    <Svg width={size || 4} height={size || 4} viewBox="0 0 4 4" fill="none">
+    <Svg
+      style={style}
+      width={size || 4}
+      height={size || 4}
+      viewBox="0 0 4 4"
+      fill="none">
       <Circle cx={2} cy={2} r={2} fill={color || '#000000'} />
     </Svg>
   );
