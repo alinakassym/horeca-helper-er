39bf479977744d9c02cbfb7941a38eb604d2066a
Alina <alinakassym@gmail.com>
2021-12-30 16:42:08 +0600

    JobCard -> PureComponent

--- src/screens/jobs/components/JobCard.js
+++ src/screens/jobs/components/JobCard.js
@@ -1,88 +1,103 @@
 import React from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
-import {PrimaryColors} from '../../../styles/colors';
-import MenuButton from '../../../components/buttons/MenuButton';
 import moment from 'moment';
 import 'moment/locale/ru';
+import PropTypes from 'prop-types';
+import {View, Text, StyleSheet} from 'react-native';
+
+// styles
+import {PrimaryColors, StatusesColors} from '../../../styles/colors';
+import {globalStyles} from '../../../styles/globalStyles';
+
+// icons
 import {IconSearch} from '../../../assets/icons/tabs/IconSearch';
+import {IconDot} from '../../../assets/icons/main/IconDot';
 
-export const JobCard = ({item, onPress, findRelevant}) => {
-  const numberWithSpaces = val => {
-    let parts = val.toString().split('.');
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-    return parts.join('.');
-  };
-  moment.locale('ru');
-  const formattedDate = date => {
-    const fd = moment(date).format('DD MMM YYYY');
-    return fd.slice(0, 1).toUpperCase() + fd.substr(1, fd.length - 1);
-  };
+// components
+import MenuButton from '../../../components/buttons/MenuButton';
+import PlainButton from '../../../components/buttons/PlainButton';
 
-  return (
-    <>
-      <View style={styles.card}>
-        <View style={[styles.row, styles.spaceBetween]}>
-          <View>
-            <Text style={styles.position}>{item.position.title}</Text>
-            <Text style={styles.location}>{item.city.title_ru}</Text>
-          </View>
-          <MenuButton />
-        </View>
-        {item.salaryMin && item.salaryMax ? (
-          <Text style={styles.salary}>
-            {numberWithSpaces(item.salaryMin)} -{' '}
-            {numberWithSpaces(item.salaryMax)} KZT
-          </Text>
-        ) : item.salaryMin ? (
-          <Text style={styles.salary}>
-            от {numberWithSpaces(item.salaryMin)} KZT
-          </Text>
-        ) : item.salaryMax ? (
-          <Text style={styles.salary}>
-            до {numberWithSpaces(item.salaryMax)} KZT
-          </Text>
-        ) : (
-          false
-        )}
-        {item.description && (
-          <Text style={styles.description}>{item.description}</Text>
-        )}
-        <TouchableOpacity activeOpacity={0.7} style={styles.btn}>
-          <IconSearch size={16} width={3} color={PrimaryColors.brand} />
-          <Text style={styles.btnText}>Найти подходящих кандидатов</Text>
-        </TouchableOpacity>
-      </View>
-      <Text style={styles.updatedAt}>
-        Обновлено {formattedDate(item.updatedAt)}
-      </Text>
-    </>
-  );
+const propTypes = {
+  item: PropTypes.object,
+  onPress: PropTypes.func,
+  findRelevant: PropTypes.func,
 };
 
+class JobCard extends React.PureComponent {
+  render() {
+    const {item, onPress, findRelevant} = this.props;
+
+    const numberWithSpaces = val => {
+      let parts = val.toString().split('.');
+      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+      return parts.join('.');
+    };
+    moment.locale('ru');
+    const formattedDate = date => {
+      const fd = moment(date).format('DD MMM YYYY');
+      return fd.slice(0, 1).toUpperCase() + fd.substr(1, fd.length - 1);
+    };
+
+    return (
+      <>
+        <View style={globalStyles.card}>
+          <View style={[globalStyles.row, globalStyles.spaceBetween]}>
+            <View>
+              <View style={[globalStyles.row, globalStyles.alignCenter]}>
+                <Text style={styles.position}>{item.position.title_ru}</Text>
+                <IconDot
+                  size={8}
+                  style={globalStyles.ml3}
+                  color={StatusesColors.green}
+                />
+              </View>
+              <Text style={styles.location}>{item.city.title_ru}</Text>
+            </View>
+            <MenuButton onPress={onPress} />
+          </View>
+          {item.salaryMin && item.salaryMax ? (
+            <Text style={styles.salary}>
+              {numberWithSpaces(item.salaryMin)} -{' '}
+              {numberWithSpaces(item.salaryMax)} KZT
+            </Text>
+          ) : item.salaryMin ? (
+            <Text style={styles.salary}>
+              от {numberWithSpaces(item.salaryMin)} KZT
+            </Text>
+          ) : (
+            item.salaryMax && (
+              <Text style={styles.salary}>
+                до {numberWithSpaces(item.salaryMax)} KZT
+              </Text>
+            )
+          )}
+          {item.description && (
+            <Text style={styles.description}>{item.description}</Text>
+          )}
+          <PlainButton
+            onPress={findRelevant}
+            style={[
+              globalStyles.mt6,
+              globalStyles.row,
+              globalStyles.alignSelfStart,
+            ]}
+            label={'Найти подходящих кандидатов'}>
+            <IconSearch
+              style={globalStyles.mr3}
+              size={16}
+              width={3}
+              color={PrimaryColors.brand}
+            />
+          </PlainButton>
+        </View>
+        <Text style={styles.updatedAt}>
+          Обновлено {formattedDate(item.updatedAt)}
+        </Text>
+      </>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
-  card: {
-    marginTop: 8,
-    padding: 20,
-    backgroundColor: PrimaryColors.white,
-  },
-  row: {
-    flexDirection: 'row',
-  },
-  spaceBetween: {
-    justifyContent: 'space-between',
-  },
-  btn: {
-    marginTop: 24,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  btnText: {
-    marginLeft: 8,
-    fontFamily: 'Inter-SemiBold',
-    fontSize: 14,
-    lineHeight: 20,
-    color: PrimaryColors.brand,
-  },
   position: {
     fontFamily: 'Inter-SemiBold',
     fontSize: 16,
@@ -121,3 +136,6 @@ const styles = StyleSheet.create({
     backgroundColor: PrimaryColors.grey4,
   },
 });
+
+JobCard.propTypes = propTypes;
+export default JobCard;
