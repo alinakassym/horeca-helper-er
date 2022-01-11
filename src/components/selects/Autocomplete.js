dbc0f7edbdd5d9b7ac68fd6a6a78843c6123aeb1
Alina <alinakassym@gmail.com>
2022-01-11 23:18:54 +0600

    fix: Autocomplete

--- src/components/selects/Autocomplete.js
+++ src/components/selects/Autocomplete.js
@@ -15,6 +15,7 @@ import {PrimaryColors} from '../../styles/colors';
 
 // icons
 import {IconClose} from '../../assets/icons/main/IconClose';
+import {IconCheck} from '../../assets/icons/main/IconCheck';
 
 // components
 import Header from '../Header';
@@ -25,6 +26,7 @@ const propTypes = {
   value: PropTypes.object,
   items: PropTypes.array,
   itemKey: PropTypes.string,
+  validIcon: PropTypes.object,
   onSelect: PropTypes.func,
   onClear: PropTypes.func,
 };
@@ -32,15 +34,16 @@ const propTypes = {
 class Autocomplete extends React.PureComponent {
   constructor(props) {
     super(props);
-    console.log({props});
     this.state = {
       searchText: '',
       visible: false,
-      filteredList: props.items,
+      filteredList: [],
     };
   }
+
   render() {
-    const {label, value, items, itemKey, onSelect, onClear} = this.props;
+    const {label, value, items, itemKey, validIcon, onSelect, onClear} =
+      this.props;
     const {searchText, visible, filteredList} = this.state;
 
     const saveHandler = selectedItem => {
@@ -73,7 +76,14 @@ class Autocomplete extends React.PureComponent {
               clearValue();
             }}
             style={styles.clearBtn}>
-            <IconClose size={20} color={PrimaryColors.grey1} />
+            <IconClose
+              style={styles.icon}
+              size={16}
+              color={PrimaryColors.grey1}
+            />
+            {validIcon || (
+              <IconCheck size={16} color={PrimaryColors.brand} width={2} />
+            )}
           </Pressable>
         </View>
       );
@@ -93,21 +103,23 @@ class Autocomplete extends React.PureComponent {
     };
 
     const getItemCount = () => {
-      return filteredList.length;
+      if (filteredList.length > 0) {
+        return filteredList.length;
+      }
+      return items.length;
     };
 
-    const getItem = (filteredList, index) => ({
-      id: filteredList[index].id,
-      title: filteredList[index].title,
-      title_ru: filteredList[index].title_ru,
+    const getItem = (lItem, index) => ({
+      id: lItem[index].id,
+      title: lItem[index].title,
+      title_ru: lItem[index].title_ru,
     });
 
     const getFilteredList = sText => {
       if (sText && sText.length >= 1) {
         const val = _.filter(items, el =>
-          _.startsWith(el.title.toLowerCase(), sText.toLowerCase()),
+          _.includes(el[itemKey].toLowerCase(), sText.toLowerCase()),
         );
-        console.log({val});
         this.setState({...this.state, searchText: sText, filteredList: val});
       } else {
         this.setState({...this.state, searchText: sText, filteredList: items});
@@ -157,7 +169,7 @@ class Autocomplete extends React.PureComponent {
             }}
           />
           <VirtualizedList
-            data={filteredList}
+            data={filteredList.length > 0 ? filteredList : items}
             renderItem={({item, index}) => <Item index={index} item={item} />}
             getItemCount={getItemCount}
             getItem={getItem}
@@ -191,8 +203,14 @@ const styles = StyleSheet.create({
   },
   clearBtn: {
     position: 'absolute',
-    right: 0,
-    bottom: 10,
+    top: 24,
+    right: 4,
+    padding: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: 4,
   },
   blockPlaceholder: {
     marginBottom: 20,
